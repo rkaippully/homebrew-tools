a552309c26c19e93decc788c16b6b65dfb7a97c5
Raghu Kaippully <rkaippully@gmail.com>
2018-06-07 15:33:44 +0530

    Gamgee 0.2.0

--- Formula/gamgee.rb
+++ Formula/gamgee.rb
@@ -1,8 +1,8 @@
 class Gamgee < Formula
   desc "Gamgee is your sidekick for managing multi-factor authentication tokens."
   homepage "https://github.com/rkaippully/gamgee"
-  url "https://github.com/rkaippully/gamgee/releases/download/v0.1.0/gamgee-v0.1.0-osx.tar.gz"
-  sha256 "5dd91452b616bdcfc60fdb3c5556daafa8a42c82c2fd4ddf8e697a0d3ddc6157"
+  url "https://github.com/rkaippully/gamgee/releases/download/v0.2.0/gamgee-v0.2.0-osx.tar.gz"
+  sha256 "3fa5d9320f1e4b266fe6cbc2f9362c3e4b46377dce6ea7d71ac5b3c1ab339316"
 
   bottle :unneeded
 
